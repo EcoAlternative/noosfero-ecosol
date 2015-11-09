11a6f38c31897d7686d2cd4428a50ed567771207
Braulio Bhavamitra <braulio@eita.org.br>
2015-11-09 13:33:41 -0300

    Change default language to es_ES

--- config/local.rb
+++ config/local.rb
@@ -1,11 +1,7 @@
 I18n.available_locales = Noosfero.available_locales
-Noosfero.default_locale = 'pt_BR'
+Noosfero.default_locale = 'es_ES'
 FastGettext.locale = Noosfero.default_locale
 FastGettext.default_locale = Noosfero.default_locale
 I18n.locale = Noosfero.default_locale
 I18n.default_locale = Noosfero.default_locale
-
-if Rails.env.development?
-  ActionMailer::Base.delivery_method = :file
-end
 
